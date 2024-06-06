2d4f2f504958f5c3af71f0111e942683b935a2d5
IkbalKulahcioglu <ikbalkulahcioglu@gmail.com>
2024-06-06 23:55:58 +0300

    Created project

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Number */
+var number = 123;
+var hexa = 0x45CF;
+var octalNum = 255;
+var binary = 57;
+console.log(number);
+console.log(hexa);
+console.log(octalNum);
+console.log(binary);
---
+++ app.ts
@@ -0,0 +1,11 @@
+/* Number */
+
+let number: number = 123;
+let hexa: number = 0x45CF;
+let octalNum: number = 0o377;
+let binary: number = 0b111001;
+
+console.log(number);
+console.log(hexa);
+console.log(octalNum);
+console.log(binary);
